3899557d8e9a3e6d768991699d06039211744313
KhrystynaYelyseyeva <play1409@gmail.com>
2020-05-13 09:32:14 +0300

    Updates

---
+++ precache-manifest.08edc3e0e01eeee59b3d374a80bf677a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1743b619099eaa5a594c583c02fb8e4b",
+    "url": "/Chucks-jokes/index.html"
+  },
+  {
+    "revision": "514a8826df7929adcbb5",
+    "url": "/Chucks-jokes/static/css/main.f1d9fe7c.chunk.css"
+  },
+  {
+    "revision": "01ab9abb15ceb9fe48d4",
+    "url": "/Chucks-jokes/static/js/2.453daa59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Chucks-jokes/static/js/2.453daa59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "514a8826df7929adcbb5",
+    "url": "/Chucks-jokes/static/js/main.6f045cd2.chunk.js"
+  },
+  {
+    "revision": "71a4e2628709968a94ce",
+    "url": "/Chucks-jokes/static/js/runtime-main.77beb5c6.js"
+  },
+  {
+    "revision": "e977fb9e726c00e0765ae53cc9a0dd66",
+    "url": "/Chucks-jokes/static/media/Vector.e977fb9e.svg"
+  },
+  {
+    "revision": "358f75fed4bb54fe11cd68ab147b7c8d",
+    "url": "/Chucks-jokes/static/media/burger.358f75fe.svg"
+  },
+  {
+    "revision": "dbd9017e3a6d90935746162a2a72431f",
+    "url": "/Chucks-jokes/static/media/burger2.dbd9017e.svg"
+  },
+  {
+    "revision": "0865c8322b3cc6f1bed5a8f1f35f00b5",
+    "url": "/Chucks-jokes/static/media/heart.0865c832.svg"
+  },
+  {
+    "revision": "f8a998d1878c0acd21eab998d9eead5c",
+    "url": "/Chucks-jokes/static/media/link.f8a998d1.svg"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/Chucks-jokes/static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/Chucks-jokes/static/media/roboto-v20-latin-regular.60fa3c06.woff"
+  }
+]);
